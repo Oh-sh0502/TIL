d80fc685ad463cd46d1b46d70116a7f71df85729
oshsage <oh12sung@naver.com>
2021-03-09 23:38:18 +0900

    20210309 is gone

---
+++ Coding Test/Individual/다이나믹 프로그래밍/예제_개미전사.py
@@ -0,0 +1,24 @@
+# 정수 N을 입력 받기(3<=n<=100)
+n = int(input())
+# 모든 식량 정보 입력 받기
+array = list(map(int, input().split()))
+
+# 앞서 계산된 결과를 저장하기 위한 DP 테이브 초기화
+d = [0] * 100
+
+# 다이나믹 프로그래밍 보텀업
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n):
+    d[i] = max(d[i-1], d[i-2]+array[i])
+
+# 계산된 결과 출력
+print(d[n-1])
+
+
+# 입력예시
+# 4
+# 1 3 1 5
+
+# 출력예시
+# 8
